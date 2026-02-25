50611ba561628bb2fd6e43a6b5fcf4a284e9034d
MikailP19 <11902920@student.pxl.be>
2026-02-25 01:14:28 +0100

    Negende Versie

--- index.js
+++ index.js
@@ -472,3 +472,17 @@ function animate() {
 }
 
 animate();
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+document.querySelectorAll('.fade-in').forEach(el => {
+    observer.observe(el);
+});
